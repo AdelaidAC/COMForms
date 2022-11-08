8e72826965eed73d66cb29a6958852e2172ca5cf
Adelaid Acevedo Cardona <adelaid.acevedo16@tectijuana.edu.mx>
2022-11-08 13:56:00 -0800

    adq

--- src/components/GeneralLiability/GeneralLiability.js
+++ src/components/GeneralLiability/GeneralLiability.js
@@ -3,6 +3,10 @@ import moment from 'moment/moment'
 import Badge from "../Badge.";
 import GarageLiability from './pages/GarageLiability';
 import SpecialEvent from './pages/SpecialEvent';
+import SBFGL from './pages/SBFGL';
+import BFGL from './pages/BFGL';
+import PNGL from './pages/PNGL';
+import CCGL from './pages/CCGL';
 
 
 export default function GeneralLiability({pages, name, address, phone, date}) {
@@ -25,6 +29,24 @@ export default function GeneralLiability({pages, name, address, phone, date}) {
         special = <><Badge text={count}/><SpecialEvent/></>;
     }
 
+    if (Object.values(pages).includes("spanishBF")) {
+        count++;
+        brokerFee = <><Badge text={count}/><SBFGL name={name} date={date}/></>;
+    } else {
+        count++;
+        brokerFee = <><Badge text={count}/><BFGL name={name} date={date}/></>;
+    }
+
+    if (Object.values(pages).includes("balanceDue")) {
+        count++;
+        balanceDue = <><Badge text={count}/><PNGL name={name} address={address} phone={phone}/></>;
+    }
+
+    if (Object.values(pages).includes("creditCard")) {
+        count++;
+        creditCard = <><Badge text={count}/><CCGL name={name} address={address}/></>;
+    }
+
     React.useEffect(() => {
         window.scrollTo(0, 0);
     }, []);
@@ -33,6 +55,11 @@ export default function GeneralLiability({pages, name, address, phone, date}) {
         <div>
             {garage}
             {special}
+            {vD}
+            {pA}
+            {brokerFee}
+            {balanceDue}
+            {creditCard}
         </div>
     );
 
